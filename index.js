f72f279bdbda910b6fb0165c769295a7245b1539
DESKTOP-HPPPRHE\Javi <javimartingar11@gmail.com>
2018-02-26 16:25:35 +0100

    server

--- index.js
+++ index.js
@@ -2,11 +2,17 @@
 
 var mongoose=require('mongoose');
 
+var app=require('./app');
+var port=process.env.PORT || 3977;
+
 mongoose.connect('mongodb://localhost:27017/curso_mean2',(err,res)=>{
     if(err){
         throw err;
     }else{
         console.log("La base de datos esta corriendo");
+        app.listen(port,function(){
+            console.log("Servidor del api rest de musica escuchando");
+        });
         
     }
 });
