ee1cd512030d87a5bf64146621d2e14ce1faf91d
DESKTOP-HPPPRHE\Javi <javimartingar11@gmail.com>
2018-03-01 15:06:21 +0100

    controlador usuario

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,9 @@
 'use strict'
+var fs=require('fs');
+var path=require('path');
 var bcrypt= require('bcrypt-nodejs');
 var User=require('../models/user');
+var jwt=require('../services/jwt');
 
 function pruebas(req,res){
     res.status(200).send({
@@ -59,6 +62,9 @@ function loginUser(req,res){
                         //devovler los datos del usuario logueado
                         if(params.gethash){
                             //devolver un token de jwt
+                            res.status(200).send({
+                                token:jwt.createToken({user})
+                            });
                         }else{
                             res.status(200).send({user});
                         }
@@ -70,8 +76,67 @@ function loginUser(req,res){
         }
     });
 }
+
+function updateUser(req,res){
+    var userId=req.params.id;
+    var update=req.body;
+
+    User.findByIdAndUpdate(userId,update,(err,userUpdated)=>{
+        if(err){
+            res.status(500).send({message:'Error al actualizar el usuario'});
+        }else{
+            if(!userUpdated){
+                res.status(404).send({message:'No se ha podido actualizar el usuario'});
+            }else{
+                res.status(200).send({user:userUpdated});
+            }
+        }
+    });
+}
+
+function uploadImage(req,res){
+    var userId=req.params.id;
+    var file_name='No subido...';
+    if(req.files.image){
+        var file_path=req.files.image.path;
+        var file_split=file_path.split('\\');
+        var file_name=file_split[2];
+        var ext_split=file_name.split('\.');
+        var file_ext=ext_split[1];
+        console.log(ext_split);
+        if(file_ext=='png' || file_ext=='jpg' || file_ext=='gif'){
+            User.findByIdAndUpdate(userId,{image:file_name},(err,userUpdated)=>{
+                if(!userUpdated){
+                    res.status(404).send({message:'No se ha podido actualizar el usuario'});
+                }else{
+                    res.status(200).send({user:userUpdated});
+                }
+    
+            });
+        }else{
+            res.status(200).send({message:'Extension del archivo no valida'});
+        }
+    }else{
+        res.status(200).send({message:'No has subido ninguna imagen'});
+    }
+}
+function getImageFile(req,res){
+    var imageFile=req.params.imageFile;
+    var path_file='./uploads/users/'+imageFile;
+    fs.exists(path_file,function(exists){
+        if(exists){
+            res.sendFile(path.resolve(path_file));
+        }else{
+            res.status(200).send({message:'No existe la imagen'});
+
+        }
+    });
+}
 module.exports={
     pruebas,
     saveUser,
-    loginUser
+    loginUser,
+    updateUser,
+    uploadImage,
+    getImageFile
 };
